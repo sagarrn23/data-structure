275bd28f5dc61a2c092f8b6091873c4820eacf3d
Sagar Chaudhari <sagarchaudhari250@gmail.com>
2020-06-03 12:31:31 +0530

    Stack complete

--- Stack/stack.js
+++ Stack/stack.js
@@ -8,33 +8,43 @@ class Stack {
     push(elem) {
         this.items[this.count] = elem;
         this.count++;
-        console.log(this.items);
-        
     }
 
+    // remove last element from the stach
     pop() {
-
+        if (!this.count) return;
+        this.items.length = this.count - 1;
+        this.count--;
     }
 
+    // get size of the stack
     size() {
-
+        return this.items.length;
     }
 
+    // get last element from stack
     peek() {
-
+        return this.items[this.items.length - 1]
     }
 
+    // check if the stack is empty
     isEmpty() {
-
+        if (!this.items.length) {
+            return true;
+        }
+        return false;
     }
 
+    // print the stack
     print() {
+        return this.items;
+    }
 
+    // clean the stack
+    clean() {
+        this.count = 0;
+        this.items.length = 0
     }
 }
 
 const stack = new Stack();
-
-stack.push(100);
-stack.push(200);
-stack.push(300);
